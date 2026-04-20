bd9636c270a1e36a749f91d1264df396f302378b
Keithclinton <keithnyaburi@gmail.com>
2026-04-20 22:51:01 +0300

    unified url

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 # Start Django app in the background
-python manage.py migrate && python manage.py runserver 0.0.0.0:8000 &
+cd django_app && python manage.py migrate && gunicorn django_app.wsgi:application --bind 0.0.0.0:8000 &
 DJANGO_PID=$!
+cd ..
 
 # Start FastAPI app in the background
-uvicorn main:app --host 0.0.0.0 --port 8001 &
+cd fastapi_app && uvicorn main:app --host 0.0.0.0 --port 8001 &
 FASTAPI_PID=$!
+cd ..
 
 # Wait for both processes
 wait $DJANGO_PID $FASTAPI_PID
