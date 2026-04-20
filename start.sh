cfc729747c25f769bfcd0df15314fd5fd97966f8
Keithclinton <keithnyaburi@gmail.com>
2026-04-20 12:31:02 +0300

    corrected paths in start.sh and corsheader installed

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Start Django app in the background
-cd django_app && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 &
+cd ppv-platform/django_app && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 &
 DJANGO_PID=$!
-cd ..
+cd ../../
 
 # Start FastAPI app in the background
-cd fastapi_app && uvicorn main:app --host 0.0.0.0 --port 8001 &
+cd ppv-platform/fastapi_app && uvicorn main:app --host 0.0.0.0 --port 8001 &
 FASTAPI_PID=$!
-cd ..
+cd ../../
 
 # Wait for both processes
 wait $DJANGO_PID $FASTAPI_PID
