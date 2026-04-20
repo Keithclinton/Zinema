2605ec0442076076cbc693cdbbe1db5af3149483
Keithclinton <keithnyaburi@gmail.com>
2026-04-20 12:10:03 +0300

    start>sh script

---
+++ ppv-platform/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Start Django app in the background
+cd django_app && python manage.py migrate && python manage.py runserver 0.0.0.0:8000 &
+DJANGO_PID=$!
+cd ..
+
+# Start FastAPI app in the background
+cd fastapi_app && uvicorn main:app --host 0.0.0.0 --port 8001 &
+FASTAPI_PID=$!
+cd ..
+
+# Wait for both processes
+wait $DJANGO_PID $FASTAPI_PID
